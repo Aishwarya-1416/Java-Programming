112bd99a64a260f9e6e4253f833fbfd043129888
Aishwarya-1416 <aishusrini1416@gmail.com>
2026-02-22 20:33:26 +0530

    Add files via upload

---
+++ Operator5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Operator5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int salary = sc.nextInt();
+        int creditScore = sc.nextInt();
+        if( salary > 25000 && creditScore > 700){
+                System.out.println("Loan Approved");
+        }
+        else{
+                System.out.println("Loan Rejected");
+        }
+    }
+}
