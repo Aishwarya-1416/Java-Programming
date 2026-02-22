19b82028ad6ee93909ac82c9932fafb09e9bb435
Aishwarya-1416 <aishusrini1416@gmail.com>
2026-02-22 20:32:24 +0530

    Add files via upload

---
+++ Operator3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Operator3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int workingHours = sc.nextInt();
+        int attendancePercentage = sc.nextInt();
+        if(workingHours > 40  && attendancePercentage > 90){
+                System.out.println("Eligible");
+        }
+        else{
+                System.out.println("Not Eligible");
+        }
+    }
+}
