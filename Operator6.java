fd6baeaa2f054603f21535aa7213d62f84fdef48
Aishwarya-1416 <aishusrini1416@gmail.com>
2026-02-23 21:35:45 +0530

    Add files via upload

---
+++ Operator6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Operator6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int familyIncome = sc.nextInt();
+        int percentage = sc.nextInt();
+        if( familyIncome < 200000 && percentage >= 75){
+                System.out.println("Scholarship Granted");
+        }
+        else{
+                System.out.println("Not Granted");
+        }
+    }
+}
