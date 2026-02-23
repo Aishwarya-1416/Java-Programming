154be5323a757370546b72696efb69355a8dd267
Aishwarya-1416 <aishusrini1416@gmail.com>
2026-02-23 21:42:00 +0530

    Add files via upload

---
+++ Operator10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Operator10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int unitsConsumed = sc.nextInt();
+        int voltageFluctuation = sc.nextInt();
+        if( unitsConsumed > 500 || voltageFluctuation == 1){
+                System.out.println("Alert");
+        }
+        else{
+                System.out.println("Normal");
+        }
+    }
+}
