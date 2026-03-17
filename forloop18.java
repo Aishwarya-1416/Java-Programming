dd94cf7dbe6357d4e6c761b7d12413a500e708ae
Aishwarya-1416 <aishusrini1416@gmail.com>
2026-03-17 22:13:34 +0530

    Add files via upload

---
+++ forloop18.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class forloop18 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int urgentCount = 0;
+        double totalRevenue = 0;
+
+        for(int i = 1; i <= n; i++) {
+
+            String name = sc.next();
+            String type = sc.next();
+            int days = sc.nextInt();
+
+            double fee = 0;
+            int discount = 0;
+
+            // Membership fee
+            if(type.equals("Basic"))
+                fee = 50;
+            else if(type.equals("Premium"))
+                fee = 100;
+            else if(type.equals("VIP"))
+                fee = 200;
+
+            // Discount based on days
+            if(days >= 45)
+                discount = 20;
+            else if(days >= 30)
+                discount = 15;
+            else if(days >= 15)
+                discount = 10;
+            else
+                discount = 0;
+
+            double finalFee = fee * (1 - discount / 100.0);
+
+            // Priority check
+            String priority;
+            if(days < 10) {
+                priority = "Urgent";
+                urgentCount++;
+            }
+            else if(days <= 30)
+                priority = "High";
+            else
+                priority = "Normal";
+
+            totalRevenue += finalFee;
+
+            System.out.println("Member: " + name);
+            System.out.println("Membership: " + type);
+            System.out.println("Days Until Expiry: " + days);
+            System.out.println("Renewal Fee: $" + fee);
+            System.out.println("Discount: " + discount + "%");
+            System.out.println("Final Fee: $" + finalFee);
+            System.out.println("Priority: " + priority);
+        }
+
+        double average = totalRevenue / n;
+
+        System.out.println("Total Members: " + n);
+        System.out.println("Urgent Renewals: " + urgentCount);
+        System.out.println("Total Renewal Revenue: $" + totalRevenue);
+        System.out.println("Average Renewal Fee: $" + average);
+    }
+}
